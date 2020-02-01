abbc580ab414a7949efcbf15d7ea2b942eb7e111
Tymko Tecca <tymko.tecca@gmail.com>
2020-01-31 20:46:42 -0500

    Mess around with naive query

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/krosantos/myomer/v2
 
 go 1.13
 
-require github.com/jackc/pgx/v4 v4.3.0 // indirect
+require (
+	github.com/jackc/pgx/v4 v4.3.0
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+)
+
 func main() {
-	print("lemon")
+	fmt.Fprintf(os.Stdout, "Attempting to connect to %v\n", os.Getenv("DB_URL"))
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DB_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to the db: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	rows, err := conn.Query(context.Background(), "select username from users")
+	defer rows.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to scan row: %v\n", err)
+		os.Exit(1)
+	}
+	for rows.Next() {
+		var username string
+		err = rows.Scan(&username)
+
+		fmt.Println(username)
+	}
 }
